730a6407a6080ffbbd5ec9f798e096618ceff833
korpikoodari <pasi@korpikoodarit.eu>
2018-06-05 17:43:42 +0300

    initial project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
+import { MenuDetailComponent } from './menu-detail/menu-detail.component';
+import { SearchComponent } from './search/search.component';
+import { StarsComponent } from './stars/stars.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarouselComponent,
+    FooterComponent,
+    NavbarComponent,
+    MenuItemComponent,
+    MenuDetailComponent,
+    SearchComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule,
