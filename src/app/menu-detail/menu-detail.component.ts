b68092e99223c7f687bd5ae0b812441ed00b6fda
korpikoodari <pasi@korpikoodarit.eu>
2018-06-05 17:45:11 +0300

    initial setup

---
+++ src/app/menu-detail/menu-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dating-menu-detail',
+  templateUrl: './menu-detail.component.html',
+  styleUrls: ['./menu-detail.component.css']
+})
+export class MenuDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
